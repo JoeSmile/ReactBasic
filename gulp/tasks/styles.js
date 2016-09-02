1d1dc09f56ccfb1e6715adbcdb4696c43bd6c1bd
guowei <guowei@wwx.loacl>
2016-09-02 09:05:46 +0800

    add gulp-plumber for styles

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -2,10 +2,12 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var connect = require('gulp-connect');
 var config = require('../config.js').sass;
+var plumber = require('gulp-plumber');
 
 gulp.task('styles', function() {
   gulp.src(config.src)
-    .pipe(sass(config.settings))
-    .pipe(gulp.dest(config.dest))
-    .pipe(connect.reload());
+      .pipe(plumber())
+      .pipe(sass(config.settings))
+      .pipe(gulp.dest(config.dest))
+      .pipe(connect.reload());
 });
